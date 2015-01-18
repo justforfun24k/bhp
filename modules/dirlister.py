bf91745efb5a0159c4929ed5ebd9e344cd65b916
justforfun24k <justforfun24k@github.com>
2015-01-18 05:49:41 -0500

    Modules modified

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,6 +1,6 @@
 import os
 
-def run(**args):
+def ran():
 	print "[*] In dirlister module"
 	files=os.listdir(".")
 
--- modules/environment.py
+++ modules/environment.py
@@ -1,5 +1,5 @@
 import os
 
-def run(**args):
+def ran():
 	print "[*] In environment module."
 	return str(os.environ)
